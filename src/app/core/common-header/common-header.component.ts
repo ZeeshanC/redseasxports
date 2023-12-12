2e0ae8f5ee9e04d62c6780775d0b074429d4b147
Jeeshan <jeeshan.choudhary@ketto.org>
2023-12-12 12:54:40 +0530

    done

--- src/app/core/common-header/common-header.component.ts
+++ src/app/core/common-header/common-header.component.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { UtilService } from '../../shared/services/util/util.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-common-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatSidenavModule, MatIconModule, MatMenuModule],
   templateUrl: './common-header.component.html',
   styleUrl: './common-header.component.scss'
 })
 export class CommonHeaderComponent {
+  isMobile: any;
+  isIos: any;
+  customLogoLink = '';
+  logoUrlLight: any;
+  @ViewChild('sidenav', { static: true }) sidenav: MatSidenav;
+  public environment = environment;
 
-  environment: any;
-  logoUrlLight: string = '';
+  constructor(
+    public utilService: UtilService,
+  ) { }
 
-  onLogoClick(e: any) {
-
+  ngOnInit() {
+    this.isMobile = this.utilService.isMobile();
   }
 
+  /**logo click listner */
+  onLogoClick(event: any) {
+
+  }
 }
